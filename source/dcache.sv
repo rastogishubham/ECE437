7e6958093761f4067341373dccf60010439a6c82
Shubham Sandeep Rastogi <rastogis@purdue.edu>
2016-10-16 15:59:11 -0400

    Create dcache, changed icache filename

---
+++ source/dcache.sv
@@ -0,0 +1,9 @@
+`include "cpu_types_pkg.vh"
+`include "datapath_cache_if.vh"
+`include "caches_if.vh"
+module icache (
+	input CLK, nRST,
+	datapath_cache_if.dcache ddcif,
+	caches_if.dcache cdcif
+);
+
