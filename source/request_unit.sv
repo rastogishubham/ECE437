b0ae412718c3b4d131b7d6862f8310ec62e81df6
Shubham Rastogi <rastogis@purdue.edu>
2017-09-15 19:45:09 -0400

    Latch instruciton in request unit

--- source/request_unit.sv
+++ source/request_unit.sv
@@ -4,43 +4,24 @@ module request_unit (
   input logic CLK, nRST,
   request_unit_if.ru reqif
 );
+
   import cpu_types_pkg::*;
+  word_t instruction;
   always_ff @(posedge CLK, negedge nRST)
   begin
 	if(!nRST)
 	begin
 		reqif.dmemWEN <= 0;
 		reqif.dmemREN <= 0;
+		instruction <= 0;
 	end
-	/*else if(reqif.ihit && reqif.dREN)
-	begin
-		reqif.dmemREN <= reqif.dREN;
-	end
-	else if(reqif.ihit && reqif.dWEN)
-	begin
-		reqif.dmemWEN <= reqif.dWEN;
-	end*/
-	/*else if(reqif.ihit)
-	begin
-		reqif.dmemREN <= reqif.dREN;
-		reqif.dmemWEN <= reqif.dWEN;
-	end
-	else if(reqif.dhit)
-	begin
-		reqif.dmemWEN <= 0;
-		reqif.dmemREN <= 0;
-	end
-	else
-	begin
-		reqif.dmemWEN <= 0;
-		reqif.dmemREN <= 0;
-	end*/
 	else
 	begin
 		if(reqif.ihit)
 		begin
 			reqif.dmemREN <= reqif.dREN;
 			reqif.dmemWEN <= reqif.dWEN;
+			instruction <= reqif.imemload;
 		end
 		else if(reqif.dhit)
 		begin
@@ -50,4 +31,5 @@ module request_unit (
 	end
   end
   assign reqif.PCEN = reqif.ihit;
+  assign reqif.instr = (reqif.ihit) ? reqif.imemload : instruction;
 endmodule
